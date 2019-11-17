76de332d106a265320d73b0bdc5aa334a449e227
TheImperion <majorXL1@yandex.ru>
2019-11-17 12:53:19 +0300

    zaliv_4_laba

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Главная страница</title>
+
+	<link rel="stylesheet" type="text/css" href="/style.css">
+	<link href="https://use.fontawesome.com/ff295dfd4d.css" media="all" rel="stylesheet">
+</head>
+<body>
+
+	<div class="header">
+		<div class="menu_header_wrapper">
+			<div class="menu_header">
+				<ul>
+					<li><a href="#">Главная</a></li>
+					<li><a href="#">Наши проекты</a></li>
+					<li><a href="#">Услуги</a></li>
+					<li><a href="#">Новости</a></li>
+					<li><a href="#">Контакты</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="banner_content">
+			<div class="title_company">Передовая IT студия</div>
+			<div class="title_banner">Мы создаем легкие решения<br>сложных задач и проблем</div>
+			<button class="btn">Наши проекты</button>
+		</div>
+	</div>
+
+	<div class="services_wrapper">
+		<div class="title_services">Мы создаем мобильные приложения<br>для крупных корпораций</div>
+		<div class="text_services">Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem<br>Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</div>
+		<div class="services_items">
+			<div class="services_element">
+				<div class="title_service">Создание сайтов</div>
+				<div class="text_service">Lorem Ipsum - это текст- "рыба", часто используе в печати и вэб-дизайне.</div>
+			</div>
+			<div class="services_element">
+				<div class="title_service">Создание сайтов</div>
+				<div class="text_service">Lorem Ipsum - это текст- "рыба", часто используе в печати и вэб-дизайне.</div>
+			</div>
+			<div class="services_element">
+				<div class="title_service">Создание сайтов</div>
+				<div class="text_service">Lorem Ipsum - это текст- "рыба", часто используе в печати и вэб-дизайне.</div>
+			</div>
+		</div>
+		<button class="btn">Все услуги</button>
+	</div>
+
+	<div class="footer">
+		<div class="menu_footer_wrapper">
+			<div class="contact_block">
+				<div class="title_menu_block">Контактная информация</div>
+				<ul>
+					<li><i class="fa fa-envelope-o" aria-hidden="true"></i><a href="#">test@test.ru</a></li>
+					<li><i class="fa fa-phone" aria-hidden="true"></i><a href="#">+8 777 555 66 99</a></li>
+					<li><i class="fa fa-map-marker" aria-hidden="true"></i><a href="#">Москва, Бульвар Ленина 33</a></li>
+				</ul>
+			</div>
+			<div class="menu_block">
+				<div class="title_menu_block">Основные ссылки</div>
+				<ul>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">Главная</a></li>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">Наши проекты</a></li>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">Наши услуги</a></li>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">Контакты</a></li>
+				</ul>
+			</div>
+			<div class="menu_block">
+				<div class="title_menu_block">Наши проекты</div>
+				<ul>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">РосНано</a></li>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">Сколково</a></li>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">Проект "Восток"</a></li>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">ТЭЦ Калининграда</a></li>
+				</ul>
+			</div>
+			<div class="menu_block">
+				<div class="title_menu_block">Социальные ссылки</div>
+				<ul>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">VK.com</a></li>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">Instagram</a></li>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">Facebook</a></li>
+					<li><i class="fa fa-chevron-right" aria-hidden="true"></i><a href="#">Twitter</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="copyright_footer">(с) 2019. Все права защищены.</div>
+	</div>
+
+
+	<script type="text/javascript" src="https://use.fontawesome.com/ff295dfd4d.js"></script>
+</body>
+</html>
